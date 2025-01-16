8410ea4dbc5577a730e76cfa1009825fa02579e9
jbh1996 <jacob.b.hopkins.96@gmail.com>
2025-01-15 16:41:12 -0800

    Test Commit

--- sponsorship_widget.py
+++ sponsorship_widget.py
@@ -10,7 +10,7 @@ class Sponsorship(QWidget):
         self.spin_box = QSpinBox()
         self.spin_box.setRange(1, 100)  # Set the range of values
         self.spin_box.setValue(1)     # Set the initial value
-        self._button = QPushButton("Delete", clicked= self.delete())
+        self._button = QPushButton("Delete", clicked= lambda: self.delete())
         self._layout.addWidget(self._name_display)
         self._layout.addWidget(self.spin_box)
         self._layout.addWidget(self.spin_box)
